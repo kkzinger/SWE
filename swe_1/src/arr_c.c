d99420c27cacc87823c302a78884c997b23511d7
Sir_Fergus <gerold@katzinger.info>
2014-03-24 00:38:41 +0100

    xcalc parse und pattern_solve funktion
    Bei Eingabe muss mit eckigen Klammern gearbeitet werden, Ansonsten muss jede klammer escapet werden

--- swe_1/src/arr_c.c
+++ swe_1/src/arr_c.c
@@ -7,7 +7,7 @@
 
 */
 
-#include <stdlib.h> //TEST
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
--- swe_2/xcalc.c
+++ swe_2/xcalc.c
@@ -19,29 +19,26 @@ int pos=-1;
 int pop(); //Funktion zum lesen vom Stack
 void push(int wert); //Funktion zum schreiben auf stack
 void print_stack(); //Funktion um Stack komplett auszugeben
+int contr_brackets(char *opstring);
+double solve_pattern(char *opstring, start, end);
+//-----------------------------------------------------------------
+//			MAIN
+//-----------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
-	int s, i, len = 0;
+	int s, i;
 
-	len = strlen(argv[1]);
+	if(contr_brackets(argv[1]) == -1) return EXIT_FAILURE;
 
-	for(i=0;i<=len;i++)
-	{
-		if(!(strchr(argv[1], '(') && strchr(argv[1], ')'))) break;
-
-		if(strchr(argv[1], '(')) push(1);
-
-		if(strchr(argv[1], ')')) 
-		{
-			s = pop();
-			if(s == -2) fprintf(stdout,"Syntax Error (Klammern)");
-		}
-	}
-
+	print_stack();
 	return 0;
 }
 
+
+//-----------------------------------------------------------------
+//			FUNCTIONS
+//-----------------------------------------------------------------
 int pop()
 {
 
@@ -63,9 +60,61 @@ void print_stack()
 {
 	int i=STACK_SIZE-1;
 
-	while(i != 0)
+	while(i >= 0)
 	{
-		printf("Pos. %d: %6d\n",i+1,stack[i]);
+		printf("Pos. %d: %6d\n",i,stack[i]);
 		i--;
 	}
+	printf("Stack Pos.: %d\n",pos);
+}
+
+int contr_brackets(char *opstring)
+{
+	int i, s, len;
+
+	len = strlen(opstring);
+
+	print_stack();
+
+	for(i=0;i<=len;i++)
+	{
+		//if(!(strchr(argv[1], '(') && strchr(argv[1], ')'))) break;
+
+		if(opstring[i] == '[') 
+		{
+			push(i+1);
+		}
+		else if(opstring[i] == ']')
+		{
+			s = pop();
+			if(s == -2)
+			{
+				fprintf(stdout,"Syntax Error (Klammern)\n");
+				return -1;
+			}
+		}
+	}
+
+	if(pos != -1)
+	{
+		fprintf(stdout,"Syntax Error (Klammern)\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+double solve_pattern(char *opstring, start, end)
+{
+	double result=0;
+	double *ptr_num, *ptr_op;
+
+	parse(*string, &ptr_num, &op)
+
+
+}
+
+void parse(char *string, double **ptr_num, double **ptr_op)
+{
+
 }
