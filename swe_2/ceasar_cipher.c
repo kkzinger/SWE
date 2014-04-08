a3e3c0dfec5952def6dc3afcf4c5d36315a9bb1f
Sir_Fergus <gerold@katzinger.info>
2014-04-08 11:27:29 +0200

    ceaser cipher fertig, eventuell noch verbesserungen an der verwendung

--- swe_2/ceasar_cipher.c
+++ swe_2/ceasar_cipher.c
@@ -11,13 +11,13 @@
 
 int main(int argc,char **argv)
 {
-	int i, d, shift_len;
+	int i, d, e, shift_len, modus;
 	char *filepath_source, *filepath_result;
+	char solved;
 	FILE *fp_source, *fp_result;
 	printf("argc %d\n",argc);
 
 	filepath_result = (char*) malloc(50);
-	strcpy(filepath_result,"result.txt");
 
 	if(argc <= 1)
 	{
@@ -38,6 +38,13 @@ int main(int argc,char **argv)
 		{
 			i++;
 			shift_len = atoi(argv[i]) % 26;
+			modus = 0;
+
+		}else if(strcmp(argv[i],"-d") == 0)
+		{
+			i++;
+			shift_len = 0;
+			modus = 1;
 		}
 
 	}
@@ -45,32 +52,80 @@ int main(int argc,char **argv)
 	printf("filepath: %s \nshift_len: %d\n",filepath_source,shift_len);
 
 	fp_source = fopen(filepath_source, "r");
-	fp_result = fopen(filepath_result, "w");
 
-	while((d=fgetc(fp_source))!=EOF)	//Scan file and count number of lines
+	if(modus == 0)
 	{
-		if(d >=65 && d <=90)
+		strcpy(filepath_result,"encoded.txt");
+		fp_result = fopen(filepath_result, "w");
+
+		while((d=fgetc(fp_source))!=EOF)	//Scan file and count number of lines
 		{
-			d -= 65;
-			d += shift_len;
-			d %= 26;
-			d += 65;
-		}else if(d >= 97 &&  d <= 122)
-		{
-			d -= 97;
-			d += shift_len;
-			d %= 26;
-			d += 97;
+			if(d >=65 && d <=90)
+			{
+				d -= 65;
+				d += shift_len;
+				d %= 26;
+				d += 65;
+			}else if(d >= 97 &&  d <= 122)
+			{
+				d -= 97;
+				d += shift_len;
+				d %= 26;
+				d += 97;
+			}
+
+			fputc(d,fp_result);
 		}
 
-		fputc(d,fp_result);
-	}
+	}else if(modus == 1)
+	{
+		strcpy(filepath_result,"result_decode.txt");
 
+		fp_result = fopen(filepath_result, "w");
+
+		shift_len = 0;
+		solved = 'n';
+
+		while(solved == 'n')
+		{
+			while((d=fgetc(fp_source))!=EOF)	//Scan file and count number of lines
+			{
+				//printf("dd\n");
+				if(d >=65 && d <=90)
+				{
+					d -= 65;
+					d += shift_len;
+					d %= 26;
+					d += 65;
+				}else if(d >= 97 &&  d <= 122)
+				{
+					d -= 97;
+					d += shift_len;
+					d %= 26;
+					d += 97;
+				}
+
+				putc(d,fp_result);
+				printf("%c",d);
+			}
+			printf("Is this the decoded text? [y/n] ");
+			fflush(stdin);
+			scanf("%s",&solved);
+			if(solved != 'y')
+			{
+				//solved = 'n';
+				shift_len++;
+				rewind(fp_source);
+				rewind(fp_result);
+			}
+		}
+
+	}
 	fclose(fp_source);
 	fclose(fp_result);
 
-//	free(filepath_source);
-//	free(filepath_result);
+	//free(filepath_source);
+	free(filepath_result);
 
 	return EXIT_SUCCESS;
 }
